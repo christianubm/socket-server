5302e1ea92350487ac7810fc1fd3fa997b2890e4
christian berdeja <christian.berdeja@inegi.org.mx>
2020-03-26 14:08:26 -0600

    seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,58 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
+
+import * as socket from '../sockets/sockets';
 
 export default class Server {
+
+    private static _instace: Server;
 
     public app: express.Application;
     public port: number;
 
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+
+    private constructor() {
         this.app =   express(); 
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer);
+
+        this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._instace || ( this._instace = new this() );
+
+    }
+
+    private escucharSockets() {
+
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', client => {
+            console.log('Cliente conectado');
+
+            // Mensajes
+            socket.mensaje( client, this.io );
+
+            //Desconectar
+            socket.desconectar( client );
+
+            // client.on('disconnect', () => {
+            //     console.log('Cliente Desconectado');
+            // })
+        });
+
     }
 
     start( callback: any ) {
-        this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
     }
 }
